b83aa517c80066a8afe7477378e8b9180e8d817f
Charles.Lee <cheoljoo@gmail.com>
2019-03-17 23:44:55 +0900

    check_merge : make merged data

--- test/check_excel_merge2.pl
+++ test/check_excel_merge2.pl
@@ -1,5 +1,6 @@
 use Spreadsheet::ParseExcel;   # 2003 excel
 
+our %data;
 my $p = Spreadsheet::ParseExcel->new();
 my $WB = $p->parse("./Workbook1.xls");
 if (!defined $WB) {
@@ -19,21 +20,44 @@ for my $WS ($WB->worksheets()) {
             print "Value       = ", $cell->value(),       "\n";
             print "Unformatted = ", $cell->unformatted(), "\n";
             print "\n";
+			$data{$row}{$col} = $cell->value();
         }
     }
 	print("===hash===\n");
 	for $key (keys %$WS){
 		print($key . "=" . $WS->{$key} . "\n");
 	}
+	print("===origin data===\n");
+	for my $i (sort keys %data){
+		for my $j (sort keys %{ $data{$i} }){
+			print("[$i:$j:$data{$i}{$j}]");
+		}
+		print("\n");
+	}
 	print("===MergedArea===\n");
 	for my $area ( @{ $WS->{MergedArea} } ){
 		print($area  . "\n");   # ARRAY...
 		print( @{ $area } . "\n");  # count
 		print($area->[0] . "  ");
-		print($area->[1] . "  ");
+		print($area->[1] . "  (" . $data{$area->[0]}{$area->[1]} . ")  ");
 		print($area->[2] . "  ");
-		print($area->[3] . "\n");
+		print($area->[3] . "  (" . $data{$area->[2]}{$area->[3]} . ")\n");
+		my $v = $data{$area->[0]}{$area->[1]};
+		for my $i ($area->[0] .. $area->[2]){
+			for my $j ($area->[1] .. $area->[3]){
+				$data{$i}{$j} = $v;
+			}
+		}
+
 	}
+}
+
+print("===merged data===\n");
+for my $i (sort keys %data){
+	for my $j (sort keys %{ $data{$i} }){
+		print("[$i:$j:$data{$i}{$j}]");
+	}
+	print("\n");
 }
 
 
